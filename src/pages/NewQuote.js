3e3fe761c2705150f818a34c2c2377e98d73b29a
Alan Grądecki <alangradecki@wp.pl>
2021-09-11 22:20:08 +0200

    dummy data has added

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,10 @@
+import QuoteForm from "../components/quotes/QuoteForm";
+
 const NewQuote = () => {
-  return <h1>New Quote Page</h1>;
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
