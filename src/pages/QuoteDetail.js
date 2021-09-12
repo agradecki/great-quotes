afd314a5f6dffa48b2b7c90f03375ebcb1829e97
Alan Grądecki <alangradecki@wp.pl>
2021-09-12 22:08:21 +0200

    nested routes has added

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -21,6 +21,13 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`}>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
